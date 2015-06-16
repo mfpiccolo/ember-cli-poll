8eb0707a0017c02eaeb8060214cf305916b97924
Mike Piccolo <mfpiccolo@gmail.com>
2015-06-15 22:09:30 -0700

    Remove bower directory import

--- index.js
+++ index.js
@@ -3,9 +3,4 @@
 
 module.exports = {
   name: 'ember-cli-poll',
-  included: function(app) {
-    this._super.included(app);
-
-    app.import(app.bowerDirectory + '/poll/dist/js/poll.js');
-  }
 };
